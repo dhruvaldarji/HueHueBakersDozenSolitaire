e7c0fe4f4e1ffc53e6c1d5a43f64f9ebcf6e4cde
Dhruval Darji <dhruvaldarji@gmail.com>
2013-09-12 14:52:11 -0400

    Created class Card.
    The Card class can be used to create objects with Suit and Value.
    Ideally we can create an sub-objects Spade, Diamond, Club, and Heart and
    have their Super class be this class. This is just to store data and get
    us started.

---
+++ HueHueBakersDozenSolitaire/HueHueBakersDozenSolitaire/Card.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HueHueBakersDozenSolitaire
+{
+    class Card
+    {
+        int value; // Num/Char (A,2,3,4,5,6,7,8,9,10,J,Q,K)
+        int suit; // Suit (Spade, Club, Heart, Diamond)
+
+        /// <summary>
+        /// Main Card Object.
+        /// </summary>
+        public Card()
+        {
+            value = 1;
+            suit = 2;
+        }
+
+        /// <summary>
+        /// Main Card Object.
+        /// </summary>
+        /// <param name="s">Provides Card Suit.</param>
+        /// <param name="v">Provides Card Value</param>
+        public Card(int s, int v)
+        {
+            suit = s;
+            value = v;
+        }
+
+        /// <summary>
+        /// Get the Suit of a Card
+        /// </summary>
+        /// <returns>suit</returns>
+        public int getSuit()
+        {
+            return suit;
+        }
+
+        /// <summary>
+        /// Set the suit of a Card
+        /// </summary>
+        /// <param name="t"></param>
+        public void setSuit(int t)
+        {
+            suit = t;
+        }
+
+        /// <summary>
+        /// Get the value of a card.
+        /// </summary>
+        /// <returns>value</returns>
+        public int getValue()
+        {
+            return value;
+        }
+
+        /// <summary>
+        /// Set the value of a card.
+        /// </summary>
+        /// <param name="v"></param>
+        public void setValue(int v)
+        {
+            value = v;
+        } 
+    }
+}
