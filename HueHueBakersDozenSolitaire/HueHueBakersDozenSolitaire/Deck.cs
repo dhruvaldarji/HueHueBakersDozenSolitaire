e48a123ed88ec0311c621299f725a938ee8b51aa
hanhtet <hhtet@spsu.edu>
2013-10-21 16:52:59 -0400

    Added Deck class
    Deck class has cardArray object that should show location of card.

---
+++ HueHueBakersDozenSolitaire/HueHueBakersDozenSolitaire/Deck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HueHueBakersDozenSolitaire
+{
+  
+    class Deck
+    {
+        Card[] cardArray;
+        int currentEmptyLocation = 0;
+        public Deck()
+        {
+            cardArray = new Card[52];
+
+        }
+        public void addCard(Card x)
+        {
+            cardArray[currentEmptyLocation] = x;
+            currentEmptyLocation++;
+        }
+    }
+}
